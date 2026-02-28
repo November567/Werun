97c82a3f5e0e095271dfd2f5c435fef997ba8a6d
November567 <sorrana.m@kkumail.com>
2026-02-28 07:58:57 +0700

    add http

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 // import หน้า register
 import 'features/auth/screens/register_screen.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
   runApp(const WeRunApp());
 }
 
